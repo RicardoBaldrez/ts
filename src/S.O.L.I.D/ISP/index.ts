c79c500bf5d3653a3b09fec368dabf762db1d194
Ricardo Barbosa Baldrez <35501635+RicardoBaldrez@users.noreply.github.com>
2024-05-22 20:14:17 +0000

    ISP - `Interface Segregation Principle`

---
+++ src/S.O.L.I.D/ISP/index.ts
@@ -0,0 +1,30 @@
+/**
+ * Interface segregation principle (Princípio da segregação de Interface)
+ * Os clientes não devem ser forçados a depender de types, interfaces ou membros
+ * abstratos que não utilizam
+ */
+
+import { Persistency } from './services/persistency';
+import { Messaging } from './services/messaging';
+import { Order } from './classes/order';
+import { ShoppingCart } from './classes/shopping-cart';
+import { Product } from './classes/product';
+import { FiftyPercentDiscount } from './classes/discount';
+
+const percentDiscount = new FiftyPercentDiscount();
+const shoppingCart = new ShoppingCart(percentDiscount);
+const messaging = new Messaging();
+const persistency = new Persistency();
+const order = new Order(shoppingCart, messaging, persistency);
+
+shoppingCart.addProduct(new Product('Kratos Funko', 50.9));
+shoppingCart.addProduct(new Product('Kratos Shirt', 70.9));
+shoppingCart.addProduct(new Product('Kratos pants', 120.9));
+// shoppingCart.clear();
+
+console.log(shoppingCart.products);
+console.log(shoppingCart.total());
+console.log(shoppingCart.totalWithDiscount());
+console.log(order.orderStatus);
+order.checkout();
+console.log(order.orderStatus);
