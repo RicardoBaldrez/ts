d87fc7ac00b7f8641f063882000bf0eabe96e245
Ricardo Barbosa Baldrez <35501635+RicardoBaldrez@users.noreply.github.com>
2024-05-27 18:12:02 +0000

    Better separating each test from the `Product class`

--- src/TestsWithJest/DIP/classes/Product/product.test.ts
+++ src/TestsWithJest/DIP/classes/Product/product.test.ts
@@ -5,16 +5,24 @@ const createSut = (name: string, price: number): Product => {
 };
 
 describe('Product', () => {
-  it('should return certinly values', () => {
+  it('should return correct type of values', () => {
+    const sut = createSut('Mouse', 49.9);
+
+    expect(typeof sut.name).toEqual('string');
+    expect(typeof sut.price).toEqual('number');
+  });
+
+  it('should have correct values', () => {
+    const sut = createSut('Mouse', 49.9);
+
+    expect(sut.name).toBe('Mouse');
+    expect(sut.price).toBeCloseTo(49.9);
+  });
+
+  it('should contain the name and price properties', () => {
     const sut = createSut('Mouse', 49.9);
 
     expect(sut).toHaveProperty('name');
     expect(sut).toHaveProperty('price');
-
-    expect(sut.name).toBe('Mouse');
-    expect(sut.price).toBeCloseTo(49.9);
-
-    expect(typeof sut.name).toEqual('string');
-    expect(typeof sut.price).toEqual('number');
   });
 });
